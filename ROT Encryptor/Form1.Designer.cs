c4aac4962c77e952b123257a21f614a3036070e3
Favas <52382725+FavasM@users.noreply.github.com>
2021-01-22 20:09:57 +0530

    initial upload

---
+++ ROT Encryptor/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ROT_Encryptor
+{
+    partial class mainUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainUI));
+            this.label1 = new System.Windows.Forms.Label();
+            this.stringBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.encryptBtn = new System.Windows.Forms.Button();
+            this.decryptBtn = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.aboutBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(59, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "String :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // stringBox
+            // 
+            this.stringBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.stringBox.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stringBox.Location = new System.Drawing.Point(165, 59);
+            this.stringBox.MaxLength = 50;
+            this.stringBox.Name = "stringBox";
+            this.stringBox.Size = new System.Drawing.Size(412, 26);
+            this.stringBox.TabIndex = 1;
+            this.stringBox.Text = "Enter String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(59, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Key   : ";
+            // 
+            // keyBox
+            // 
+            this.keyBox.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyBox.Location = new System.Drawing.Point(165, 130);
+            this.keyBox.MaxLength = 2;
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(412, 26);
+            this.keyBox.TabIndex = 3;
+            this.keyBox.Text = "0 - 26";
+            // 
+            // encryptBtn
+            // 
+            this.encryptBtn.BackColor = System.Drawing.Color.RoyalBlue;
+            this.encryptBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.encryptBtn.Font = new System.Drawing.Font("Cascadia Code", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.encryptBtn.ForeColor = System.Drawing.Color.White;
+            this.encryptBtn.Location = new System.Drawing.Point(184, 233);
+            this.encryptBtn.Name = "encryptBtn";
+            this.encryptBtn.Size = new System.Drawing.Size(106, 42);
+            this.encryptBtn.TabIndex = 4;
+            this.encryptBtn.Text = "Encrypt";
+            this.encryptBtn.UseVisualStyleBackColor = false;
+            this.encryptBtn.Click += new System.EventHandler(this.encryptBtn_Click);
+            // 
+            // decryptBtn
+            // 
+            this.decryptBtn.BackColor = System.Drawing.Color.RoyalBlue;
+            this.decryptBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.decryptBtn.Font = new System.Drawing.Font("Cascadia Code", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decryptBtn.ForeColor = System.Drawing.Color.White;
+            this.decryptBtn.Location = new System.Drawing.Point(360, 233);
+            this.decryptBtn.Name = "decryptBtn";
+            this.decryptBtn.Size = new System.Drawing.Size(109, 43);
+            this.decryptBtn.TabIndex = 5;
+            this.decryptBtn.Text = "Decrypt";
+            this.decryptBtn.UseVisualStyleBackColor = false;
+            this.decryptBtn.Click += new System.EventHandler(this.decryptBtn_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.BackColor = System.Drawing.Color.RoyalBlue;
+            this.resultBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.resultBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.resultBox.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultBox.ForeColor = System.Drawing.Color.White;
+            this.resultBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.resultBox.Location = new System.Drawing.Point(65, 326);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
+            this.resultBox.Size = new System.Drawing.Size(520, 19);
+            this.resultBox.TabIndex = 6;
+            this.resultBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.RoyalBlue;
+            this.exitBtn.Font = new System.Drawing.Font("Cascadia Code", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.ForeColor = System.Drawing.Color.White;
+            this.exitBtn.Location = new System.Drawing.Point(534, 385);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn.TabIndex = 7;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // aboutBtn
+            // 
+            this.aboutBtn.BackColor = System.Drawing.Color.RoyalBlue;
+            this.aboutBtn.Font = new System.Drawing.Font("Cascadia Code", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutBtn.ForeColor = System.Drawing.Color.White;
+            this.aboutBtn.Location = new System.Drawing.Point(24, 385);
+            this.aboutBtn.Name = "aboutBtn";
+            this.aboutBtn.Size = new System.Drawing.Size(75, 23);
+            this.aboutBtn.TabIndex = 8;
+            this.aboutBtn.Text = "About";
+            this.aboutBtn.UseVisualStyleBackColor = false;
+            this.aboutBtn.Click += new System.EventHandler(this.aboutBtn_Click);
+            // 
+            // mainUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(631, 423);
+            this.Controls.Add(this.aboutBtn);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.decryptBtn);
+            this.Controls.Add(this.encryptBtn);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.stringBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ROT Encrypter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox stringBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.Button encryptBtn;
+        private System.Windows.Forms.Button decryptBtn;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button aboutBtn;
+    }
+}
+
---
+++ ROT Encryptor/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROT_Encryptor
+{
+    public partial class mainUI : Form
+    {
+        public mainUI()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void decryptBtn_Click(object sender, EventArgs e)
+        {
+            string encryptedString = stringBox.Text;
+            int shiftKey = Convert.ToInt32(keyBox.Text);
+
+            string decryptedString = "";
+            char whitespace = ' ';
+
+            for (int i = 0; i < encryptedString.Length; i++)
+            {
+                if (encryptedString[i] == whitespace)
+                {
+                    decryptedString += whitespace;
+                    continue;
+                }
+
+                int decryptedValue = (int)encryptedString[i] - shiftKey;
+                char decryptedChar = (char)decryptedValue;
+
+                decryptedString += decryptedChar;
+            }
+
+            if (shiftKey < 0 || shiftKey > 26)
+                resultBox.Text = "Enter Key between 0 to 26";
+            else
+                resultBox.Text = decryptedString;
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void aboutBtn_Click(object sender, EventArgs e)
+        {
+            _ = MessageBox.Show(@"Tool Name : ROT Encryptor
+
+Author Name : Favas M
+
+Date : 22/01/2021
+
+Version : 1.0
+
+  
+         Thanks For Using");
+        }
+
+        private void encryptBtn_Click(object sender, EventArgs e)
+        {
+            string stringValue = stringBox.Text;
+            int shiftKey = Convert.ToInt32(keyBox.Text);
+
+            string encryptedString = "";
+            char whiteSpace = ' ';
+
+            for (int i = 0; i < stringValue.Length; i++)
+            {
+                if (stringValue[i] == whiteSpace)
+                {
+                    encryptedString += whiteSpace;
+                    continue;
+                }
+
+                int encryptedValue = (int) stringValue[i] + shiftKey ;
+                char encryptedChar = (char) encryptedValue;
+                
+                encryptedString += encryptedChar;
+            }
+
+            if (shiftKey < 0 || shiftKey > 26)
+                resultBox.Text = "Enter Key between 0 to 26";
+            else
+                resultBox.Text = encryptedString;
+        }
+    }
+}
